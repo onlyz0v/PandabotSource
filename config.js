1eda101f529b0d1f8d67d20adb3141a14f906ceb
onlyz0v <infxtr47@gmail.com>
2025-05-01 11:47:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Dinliyor", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/NgY8JFFD5j",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
+      name: "onlyzov",
+      password: "onlyzov",
       host: "193.226.78.187",
       port:  9372,
       secure: false
